1e04d9dd5e8b1f977bf52e98ab31ed6eb148978a
davidcf97 <davidcasal1997@gmail.com>
2023-07-27 14:06:45 +0200

    Improve header styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,22 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { HelmetDatoCms } from 'gatsby-source-datocms';
 import Link from 'gatsby-link';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 import DropdownMenu from '../components/dropdown';
 
 const Header = ({ site, seo }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      datoCmsAsset(filename: { eq: "logo.png" }) {
+        fluid(maxWidth: 300, imgixParams: { fm: "png" }) {
+          ...GatsbyDatoCmsFluid
+        }
+      }
+    }
+  `);
+
   const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const handleDropdownToggle = () => {
@@ -23,11 +35,11 @@ const Header = ({ site, seo }) => {
               <div className={`menu-icon__bar ${isDropdownOpen ? 'open' : ''}`}></div>
               <div className={`menu-icon__bar ${isDropdownOpen ? 'open' : ''}`}></div>
             </div>
-            <h1 className="Header__title">
+            <div className="Header__title">
               <Link data-text={site.globalSeo.siteName} to="/">
-                {site.globalSeo.siteName}
+                <Img fluid={data.datoCmsAsset.fluid} loading="lazy" />
               </Link>
-            </h1>
+            </div>
             <div className={`Header__summary snipcart-summary snipcart-checkout ${isDropdownOpen ? 'open' : ''}`}>
               <div className="Header__summary__title">
                 <ShoppingCartRoundedIcon data-item-count className="cart-icon" />
@@ -35,7 +47,7 @@ const Header = ({ site, seo }) => {
               </div>
             </div>
           </div>
-          {isDropdownOpen && <DropdownMenu/>}
+          {isDropdownOpen && <DropdownMenu />}
         </div>
       </div>
     </>
